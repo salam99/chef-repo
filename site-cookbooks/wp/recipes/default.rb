d626d927fa838bf5f6a6eaa02442f390edc3ab9a
Cloud Genius <user@cloudgenius.workstation>
2016-07-07 10:09:13 -0700

    updates

--- site-cookbooks/wp/recipes/default.rb
+++ site-cookbooks/wp/recipes/default.rb
@@ -28,9 +28,9 @@ bash 'install_wp' do
     echo "You do not seem to have WordPress installed already"
     echo "so I am going to install it for you"
 
-    wget https://wordpress.org/wordpress-4.2.2.tar.gz
-    tar xfz wordpress-4.2.2.tar.gz
-    rm -f wordpress-4.2.2.tar.gz
+    wget https://wordpress.org/wordpress-4.5.3.tar.gz
+    tar xfz wordpress-4.5.3.tar.gz
+    rm -f wordpress-4.5.3.tar.gz
 
     rm -rf /var/www
     mv wordpress /var/www
